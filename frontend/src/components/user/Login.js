5c4bac0f8acb54323d0c962a2eb25b318e473660
RyanD <ryanperera0531@gmail.com>
2024-06-28 17:30:29 +0530

    Login Page Check

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -73,7 +73,7 @@ const Login = () => {
                     marginBottom: "2rem",
                   }}
                 >
-                  Login
+                  Ryan Perera
                 </h1>
                 <div className="form-group">
                   <label htmlFor="email_field">Email</label>
